647d70c0a2eaeb661e0b7bbe918d5b890945d66e
Unizzu <nathankylegalang@gmail.com>
2022-10-30 02:54:00 +1100

    Pac and cherry move now

---
+++ Assets/Scripts/CherryController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryController : MonoBehaviour
+{
+    private float cherrySpeed = 3.0f;
+    private Renderer cherryRender;
+    private float randomX1;
+    private float randomX2;
+    private float randomY1;
+    private float randomY2;
+    private int binaryChoice;
+    private Vector3 Pos1;
+    private Vector3 Pos2;
+    private Vector3 StartPos;
+    private Vector3 EndPos;
+    
+    // reference:
+    // lowerBound = x: -12.95 y: -4.99
+    // upperBound = x: 10.37 y: 4.99
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCherry());
+        cherryRender = gameObject.GetComponent<Renderer>();
+        cherryRender.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = cherrySpeed * Time.deltaTime;
+        if(cherryRender.enabled)
+        {
+            if(Vector3.Distance(StartPos, EndPos) > 0.5f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, EndPos, step);
+            }
+            else if (Vector3.Distance(StartPos, EndPos) <= 0.5f)
+            {
+                transform.position = EndPos;
+                cherryRender.enabled = false;
+            }
+        }
+    }
+
+    IEnumerator SpawnCherry()
+    {
+        yield return new WaitForSeconds(10f);
+        Randomizer();
+        //Debug.Log("here come da cherry");
+        cherryRender.enabled = true;
+        transform.position = StartPos;
+        StartCoroutine(SpawnCherry());
+    }
+
+    private void Randomizer()
+    {
+        binaryChoice = Random.Range(0, 2);
+        if (binaryChoice == 0)
+        {
+            randomX1 = Random.Range(-13f, 11f);
+            randomX2 = Random.Range(-13f, 11f);
+            Pos1 = new Vector3(randomX1, -5.5f, 0f);
+            Pos2 = new Vector3(randomX2, 5.5f, 0f);
+        }
+        else
+        {
+            randomY1 = Random.Range(-5.5f, 5.5f);
+            randomY2 = Random.Range(-5.5f, 5.5f);
+            Pos1 = new Vector3(-13f, randomY1, 0f);
+            Pos2 = new Vector3(11f, randomY2, 0f);
+        }
+        binaryChoice = Random.Range(0, 2);
+        if (binaryChoice == 0)
+        {
+            StartPos = Pos1;
+            EndPos = Pos2;
+        }
+        else
+        {
+            EndPos = Pos1;
+            StartPos = Pos2;
+        }
+    }
+}
--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -5,25 +5,128 @@ using UnityEngine.Tilemaps;
 
 public class PacStudentController : MonoBehaviour
 {
-    public Grid mapGrid;
-    public Tilemap mapTile;
+    [SerializeField] public Grid mapGrid;
+    [SerializeField] private Tilemap mapTile;
+    [SerializeField] private Transform particleSpawner;
+    [SerializeField] private GameObject partSys;
 
-    private Vector3Int StartPos;
+    private Animator anim;
+    private AudioSource pacSound;
+    private Vector3Int[] NextPosArray = new Vector3Int[] { new Vector3Int(0, 0, 0), new Vector3Int(-1, 0, 0), new Vector3Int(1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(0, -1, 0) };
+    private Quaternion[] DustRotation = new Quaternion[] { new Quaternion(0, 0, 0, 1), new Quaternion(0, 90, 0, 1), new Quaternion(0, -90, 0, 1), new Quaternion(90, 0, 0, 1), new Quaternion(-90, 0, 0, 1)};
+    public enum Direction {None, Left, Right, Up, Down};
+    private Direction lastDirection = Direction.None;
+    private KeyCode lastInput;
+    private KeyCode currentInput;
+    private TileBase NextTile;
+    private Vector3Int StartPos = new Vector3Int(-17, 7, 0);
+    private Vector3Int CurrentPos;
+    private Vector3Int TargetPos;
+    private Vector3Int NextPos;
+    private float pacSpeed = 1.5f;
+    private bool isLerping;
     // Start is called before the first frame update
     void Start()
     {
-        StartPos = new Vector3Int(-17, 7, 0);
-        transform.position = mapTile.GetCellCenterWorld(StartPos);
+        anim = GetComponent<Animator>();
+        pacSound = GetComponent<AudioSource>();
+
+        CurrentPos = StartPos;
+        
+        //Debug.Log(CurrentPos);
+        //Debug.Log(TargetPos);
+        transform.position = mapTile.GetCellCenterWorld(CurrentPos);
+        StartCoroutine(dustBehavior());
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        PlayWalk();
+        float step = pacSpeed * Time.deltaTime;
+        if(!isLerping)
+        {
+            TargetPos = CurrentPos + NextPosArray[(int)lastDirection];
+            NextPos = TargetPos + NextPosArray[(int)lastDirection];
+            anim.speed = 0;
+        }
+        NextTile = mapTile.GetTile(TargetPos);
+        if (NextTile.name == "Tiles_0" || NextTile.name == "Tiles_5" || NextTile.name == "Tiles_6")
+        {
+            if (Vector3.Distance(transform.position, mapTile.GetCellCenterWorld(TargetPos)) > 0.01f)
+            {
+                anim.speed = 1;
+                anim.SetInteger("Direction", (int)lastDirection);
+                transform.position = Vector3.MoveTowards(transform.position, mapTile.GetCellCenterWorld(TargetPos), step);
+                isLerping = true;
+            }
+            else if (Vector3.Distance(transform.position, mapTile.GetCellCenterWorld(TargetPos)) <= 0.01f)
+            {
+                transform.position = mapTile.GetCellCenterWorld(TargetPos);
+                currentInput = lastInput;
+                CurrentPos = TargetPos;
+                TargetPos = NextPos;
+                anim.speed = 0;
+                isLerping = false;
+            }
+        }
+        else
+        {
+            lastDirection = Direction.None;
+        }
         
     }
 
-    void FixedUpdate()
+    private void PlayWalk()
     {
-        
+        if(lastDirection != Direction.None)
+        {
+            if (!pacSound.isPlaying)
+            {
+                pacSound.Play();
+            }
+        }
+        else
+        {
+            pacSound.Stop();
+        }
+    }
+    private void GetInput()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            lastInput = KeyCode.A;
+            lastDirection = Direction.Left;
+            //Debug.Log(NextTile.name);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lastInput = KeyCode.D;
+            lastDirection = Direction.Right;
+            //Debug.Log(NextTile.name);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lastInput = KeyCode.W;
+            lastDirection = Direction.Up;
+            //Debug.Log(NextTile.name);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            lastInput = KeyCode.S;
+            lastDirection = Direction.Down;
+            //Debug.Log(NextTile.name);
+        }
+    }
+
+    IEnumerator dustBehavior()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if(isLerping)
+        {
+            Instantiate(partSys, transform.position, DustRotation[(int)lastDirection], particleSpawner);
+        }
+        StartCoroutine(dustBehavior());
     }
 }
